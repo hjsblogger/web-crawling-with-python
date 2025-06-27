e1846d84274a17cb1c57b70aebdd7406d3972cf9
Himanshu Jagdish Sheth <himanshu.sheth@gmail.com>
2025-06-27 12:07:30 +0530

    Removal of comments

--- main/main_crawler.py
+++ main/main_crawler.py
@@ -1,6 +1,4 @@
 # Took some cues from https://github.com/xukai92/crawlerfromscratch
-# and https://www.scrapingbee.com/blog/
-# crawling-python/#building-a-simple-web-crawler-in-python-from-scratch
 
 import sys
 sys.path.append(sys.path[0] + "/..")
